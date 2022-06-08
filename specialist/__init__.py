4e091bdc65ea9b369c90007235bef7ae29e8a751
Nadir Chowdhury <chowdhurynadir0@outlook.com>
2022-06-08 22:15:31 +0000

    move specialist into package and support json arg

---
+++ specialist/__init__.py
@@ -0,0 +1,20 @@
+"""Visualize CPython 3.11's specializing, adaptive interpreter."""
+import sys
+import types
+import typing
+
+if sys.version_info < (3, 11) or sys.implementation.name != "cpython":
+    raise RuntimeError("Specialist only supports CPython 3.11+!")
+CODE = set()
+
+
+@sys.addaudithook
+def audit_imports(event: str, args: "typing.Sequence[object]") -> None:
+    """Intercept all exec() calls and grab a reference to the code they execute.
+
+    This is the only way I know of to actually get ahold of module-level code
+    objects without modifying the code being run.
+    """
+    match event, args:
+        case "exec", [types.CodeType(co_name="<module>") as code]:
+            CODE.add(code)
---
+++ specialist/writers.py
@@ -0,0 +1,82 @@
+import colorsys
+import dataclasses
+import html
+import json
+import typing
+
+from .stats import Stats
+
+
+class Writer(typing.Protocol):
+    def add(self, source: str, stats: "Stats") -> None:
+        ...
+
+    def emit(self) -> str:
+        ...
+
+
+class HTMLWriter(Writer):
+    """Write HTML for a source code view."""
+
+    def __init__(self, *, blue: bool, dark: bool) -> None:
+        self._blue = blue
+        self._dark = dark
+        background_color, color = ("black", "white") if dark else ("white", "black")
+        self._parts = [
+            "<!doctype html>",
+            "<html>",
+            "<head>",
+            "<meta http-equiv='content-type' content='text/html;charset=utf-8'/>",
+            "</head>",
+            f"<body style='background-color:{background_color};color:{color}'>",
+            "<pre>",
+        ]
+
+    def add(self, source: str, stats: "Stats") -> None:
+        """Add a chunk of code to the output."""
+        color = self._color(stats)
+        attribute = "color" if self._dark else "background-color"
+        source = html.escape(source)
+        if color != "#ffffff":
+            source = f"<span style='{attribute}:{color}'>{source}</span>"
+        self._parts.append(source)
+
+    def emit(self) -> str:
+        """Emit the HTML."""
+        return "".join([*self._parts, "</pre></body></html>"])
+
+    def _color(self, stats: "Stats") -> str:
+        """Compute an RGB color code for this chunk."""
+        quickened = stats.specialized + stats.adaptive
+        if not quickened:
+            return "#ffffff"
+        # Red is 0/3, green is 1/3. This gives a hue along the red-green gradient
+        # that reflects the hit rate:
+        hue = 1 / 3 * stats.specialized / quickened
+        if self._blue:
+            # This turns our red-green (0/3 to 1/3) gradient into a red-blue (0/3 to
+            # -1/3) gradient:
+            hue = -hue
+        lightness = max(1 / 2, stats.unquickened / (quickened + stats.unquickened))
+        # Always fully saturate the color:
+        saturation = 1
+        rgb = colorsys.hls_to_rgb(hue, lightness, saturation)
+        return f"#{int(255 * rgb[0]):02x}{int(255 * rgb[1]):02x}{int(255 * rgb[2]):02x}"
+
+
+class JSONWriter(Writer):
+    def __init__(self, *, indent: int | str | None = None) -> None:
+        self._indent = indent
+        self._data = []
+
+    def add(self, source: str, stats: "Stats") -> None:
+        self._data.append(
+            {
+                "source": source,
+                "stats": dataclasses.asdict(stats),
+            }
+        )
+
+    def emit(self) -> str:
+        """Emit the JSON data"""
+        return json.dumps({"data": self._data}, indent=self._indent)
